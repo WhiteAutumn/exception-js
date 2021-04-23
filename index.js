b00345c20e3bb2a4bd4f7530713cd06b2522b0a1
White Autumn <white@autumn.dev>
2021-04-23 18:53:59 +0200

    Add logic to handle causes that are not errors

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ export default class Exception extends Error {
 
     this.name = "Error";
 
-    if (cause != null) {
-      this.stack += "\nCaused by: " + error.stack;
+    if (cause == null) {
+      return;
+    }
+
+    this.stack += "\nCaused by: ";
+
+    if (cause instanceof Error) {
+      this.stack += cause.stack;
+    }
+    else {
+      this.stack += cause.toString();
     }
   }
 }
